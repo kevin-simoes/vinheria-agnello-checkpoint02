6faa404883f999e9c4d28fe0b328abf7303cc280
Luan Sá Muniz <luansamunizsf@gmail.com>
2026-05-10 14:55:21 -0300

    feat:adicionado contadores

--- src/js/register.js
+++ src/js/register.js
@@ -2,84 +2,104 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const botao = document.getElementById("btnCadastro");
 
-  function perguntar(mensagem) {
-
-    const resposta = prompt(mensagem);
-
-    if (resposta === null) {
-      alert("Cadastro cancelado.");
-      return null;
-    }
-
-    return resposta;
-  }
-
-  function verificarEstoque(quantidade) {
-
-    if (quantidade < 5) {
-      alert("Estoque baixo!");
-      console.log("Estoque baixo! Quantidade em estoque:", quantidade);
-    } else {
-      alert("Estoque suficiente.");
-      console.log("Estoque suficiente. Quantidade em estoque:", quantidade);
-    }
-
-  }
-
-  function perguntarQuantidade() {
-
-    let quantidade;
-
-    while (true) {
-
-      quantidade = perguntar("Digite a quantidade em estoque:");
-
-      if (quantidade === null) {
-        return null;
-      }
-
-      quantidade = Number(quantidade);
-
-      if (!isNaN(quantidade)) {
-        return quantidade;
-      }
-
-      alert("Digite apenas números!");
-
-    }
-
-  }
 
   botao.addEventListener("click", () => {
 
     let continuar = true;
+    let qtdcadastros=0;
+    let nomeantigo;
+    let antigo=2026;
+    let estoquebaixo=0;
 
-    while (continuar) {
+    Cadastro();
 
-      const nome = perguntar("Digite o nome do vinho:");
-      if (nome === null) return;
+    function perguntar(mensagem) {
 
-      const tipo = perguntar("Digite o tipo do vinho (Tinto, Branco, Rosé):");
-      if (tipo === null) return;
+      const resposta = prompt(mensagem);
 
-      const ano = perguntar("Digite o ano da safra:");
-      if (ano === null) return;
+      if (resposta === null) {
+        alert("Cadastro cancelado.");
+        return null;
+      }
 
-      const quantidade = perguntarQuantidade();
-      if (quantidade === null) return;
+      return resposta;
+    }    
 
-      verificarEstoque(quantidade);
-
-      console.log("Nome:", nome);
-      console.log("Tipo:", tipo);
-      console.log("Ano:", ano);
+    function antigos(ano,nome){
+      if(ano<antigo){
+        antigo=ano;
+        nomeantigo=nome;
+      }
+      console.log("O Vinho mais antigo é o :",nomeantigo);
+    }
     
+    function verificarEstoque(quantidade) {
 
-      continuar = confirm("Deseja cadastrar outro vinho?");
+      if (quantidade < 5) {
+        alert("Estoque baixo!");
+        estoquebaixo++;
+        console.log("Quantidade de Vinhos com Estoque baixo:", estoquebaixo);
+        console.log("Estoque baixo! Quantidade em estoque:", quantidade);
+      } else {
+        alert("Estoque suficiente.");
+        console.log("Estoque suficiente. Quantidade em estoque:", quantidade);
+      }
+
     }
 
-    alert("Cadastros finalizados!");
+    function perguntarQuantidade() {
 
+      let quantidade;
+
+      while (true) {
+
+        quantidade = perguntar("Digite a quantidade em estoque:");
+
+        if (quantidade === null) {
+          return null;
+        }
+
+        quantidade = Number(quantidade);
+
+        if (!isNaN(quantidade)) {
+          return quantidade;
+        }
+
+        alert("Digite apenas números!");
+
+      }
+
+    }    
+    function Cadastro(){
+      while (continuar) {
+
+        qtdcadastros++;
+        console.log("Quantidade de Cadastros:",qtdcadastros);
+
+        const nome = perguntar("Digite o nome do vinho:");
+        if (nome === null) return;
+
+        const tipo = perguntar("Digite o tipo do vinho (Tinto, Branco, Rosé):");
+        if (tipo === null) return;
+
+        const ano = perguntar("Digite o ano da safra:");
+        antigos(ano,nome);
+        if (ano === null) return;
+
+        const quantidade = perguntarQuantidade();
+        if (quantidade === null) return;
+
+        verificarEstoque(quantidade);
+
+        console.log("Nome:", nome);
+        console.log("Tipo:", tipo);
+        console.log("Ano:", ano);
+      
+        continuar = confirm("Deseja cadastrar outro vinho?");
+      }
+      alert("Cadastros finalizados!");
+    }
+    
   });
 
 });
